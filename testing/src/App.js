5384295b3540bb41814fda248d25f3c9e51da9aa
Caden F <cadenmfink@gmail.com>
2024-09-10 13:34:51 -0700

    Basic Dev styling

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,9 +1,10 @@
 import { UI } from "caden";
+import "./App.css";
 import "caden/lib/styles/css/index.css";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <UI.Buttons.Submit>Submit</UI.Buttons.Submit>
     </div>
   );
