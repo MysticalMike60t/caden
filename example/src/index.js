18890e3b0dc325a05843cf6c402cbd0bf6da3da9
Caden F <cadenmfink@gmail.com>
2024-09-09 18:14:03 -0700

    Added Testing Environment

---
+++ example/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Buttons, Layout } from '../lib'; // Import your components here
+
+const App = () => (
+  <div>
+    <Layout.Page.Home />
+    <Buttons.Submit />
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
